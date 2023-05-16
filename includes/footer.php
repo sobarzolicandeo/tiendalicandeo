6c6998ee6c6a29fc523490d74ea0e91e032aa05e
sobarzolicandeo <sobarzolicandeo@gmail.com>
2023-05-16 18:07:01 -0400

    Base proyecto

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" style="background-color: darkgray">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2023 Brought to You By <a href="https://www.instagram.com/tienda_licandeo/">TIENDA LICANDEO</a></strong>
+    </div>
+</footer>
