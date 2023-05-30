136523568074748ab6e5d33ca54c4602d40505e6
sobarzolicandeo <sobarzolicandeo@gmail.com>
2023-05-29 23:06:31 -0400

    Actualización enlaces

--- admin/users.php
+++ admin/users.php
@@ -11,7 +11,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Usuario
+        Usuarios
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
