59fa60d0bb9a0f91429c3d40e4482f576ba2523f
sobarzolicandeo <sobarzolicandeo@gmail.com>
2023-05-29 21:47:42 -0400

    Cambios a español

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>Desarrollado por Tienda Licandeo</b>
     </div>
-    <strong> Creado por <a href="https://www.instagram.com/tienda_licandeo/">TIENDA LICANDEO</a></strong>
+    <strong> Creado por <a href="https://www.instagram.com/tienda_licandeo/">Tienda Licandeo</a></strong>
 </footer>
