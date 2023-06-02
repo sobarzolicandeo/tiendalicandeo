819ba518b68f5f75e0e83a530d4d029c2895e40e
sobarzolicandeo <sobarzolicandeo@gmail.com>
2023-06-01 21:02:38 -0400

    Cambios en estilo

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 <header class="main-header">
-  <nav class="navbar navbar-static-top">
+  <nav class="navbar navbar-static-top" style="background-color: #513b0e;">
     <div class="container">
       <div class="navbar-header">
         <a href="index.php" class="navbar-brand"><b>Tienda Licandeo</b></a>
